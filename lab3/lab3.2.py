b1c497e68b06081e47dab314edbd54c49e7ef28c
AlisherReh <alisher.toktarkhan@gmail.com>
2023-01-29 23:22:16 +0600

    lab3

---
+++ lab3/lab3.2.py
@@ -0,0 +1,30 @@
+#1
+print("Вывод 1")
+for i in range(10):
+    print(i)
+
+print()
+
+#2
+print("Ввод")
+n = int(input())
+m = int(input())
+print("")
+print("Вывод 2")
+for i in range(n, m+1):
+    print(i)
+
+print()
+
+#3
+print("Вывод3")
+for i in range(4, 20, 2):
+    print(i)
+
+
+print()
+
+#4
+print("Вывод4")
+for i in range(5, 0, -1):
+    print(i)
